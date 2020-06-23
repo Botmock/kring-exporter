543c4ad85c06461a30ef2240b9d5016eb0ac94c7
Kevin Donahue <nonnontrivial@gmail.com>
2020-06-23 11:45:03 -0400

    add schema map

--- exporter.ts
+++ exporter.ts
@@ -5,17 +5,49 @@ import {
   Botmock,
 } from "@botmock/export";
 
+namespace MSBotFramework {
+  export interface SchemaContent { }
+}
+
 export class KringExporter extends BaseExporter {
-  #createSchemaForContentBlock = (block: Botmock.Block) => { };
+  #schemaMap: Map<string, string> = new Map([
+    ["card", ""],
+    ["button", ""],
+    ["quick_replies", ""],
+  ]);
+  #createSchemaForContentBlock = (block: Botmock.Block): MSBotFramework.SchemaContent => {
+    return {};
+  };
   #createTemplatesFromProjectResources = (resources: Resources) => {
     return {};
   };
+  /**
+   * Generates `.lg` file content and `.json` schema content for applicable blocks.
+   * @param resources Object containing botmock resources.
+   */
   #rootTransformation = (resources: Resources): DataTransformation => {
+    const payloadsContainingSchemaContent = (resources.board.board.messages as Botmock.Message[])
+      .reduce((payloads, message) => {
+        const valuesByPlatform = Object.values(message.payload);
+        const schemaContent: MSBotFramework.SchemaContent[] = valuesByPlatform
+          .flatMap(value => Object.values(value).flatMap(b => b.blocks))
+          .filter(block => this.#schemaMap.has(block.component_type))
+          .map(this.#createSchemaForContentBlock);
+        return {
+          ...payloads,
+          [message.message_id]: schemaContent,
+        };
+      }, {});
     return [
       {
         filename: `${resources.project.name}.lg`,
         data: this.#createTemplatesFromProjectResources(resources),
       },
+      ...Object.keys(payloadsContainingSchemaContent).map(key => ({
+        filename: `${key}.json`,
+        // @ts-ignore
+        data: payloadsContainingSchemaContent[key],
+      })),
     ];
   };
   /**
