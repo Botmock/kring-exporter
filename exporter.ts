0665b1e7cfdee2df51a96e25443400b0b1b04dc8
Kevin Donahue <nonnontrivial@gmail.com>
2020-06-23 21:54:54 -0400

    add generate schema content private field

--- exporter.ts
+++ exporter.ts
@@ -8,6 +8,7 @@ import { v4 } from "uuid";
 import { MSBotFramework } from "./types";
 
 export class KringExporter extends BaseExporter {
+  #templateMap: Map<string, string> = new Map();
   #schemaMap: Map<string, string> = new Map([
     [Botmock.Component.button, MSBotFramework.BodyTypes.CHOICE],
     [Botmock.Component.quick_replies, MSBotFramework.BodyTypes.CHOICE],
@@ -38,38 +39,44 @@ export class KringExporter extends BaseExporter {
       ],
     };
   };
-  #createTemplatesFromProjectResources = (resources: Resources) => {
-    return {};
+  #createTemplatesFromProjectResourcesAndSchemaContent = (resources: Resources, schemaContent: {}) => {
+    return (resources.board.board.messages as Botmock.Message[]).reduce((obj, message) => {
+      return {
+        ...obj,
+        [message.message_id]: ["x"]
+      };
+    }, {});
+  };
+  #generateSchemaContent = (messages: Botmock.Message[]) => {
+    return messages.reduce((payloads, message) => {
+      const valuesByPlatform = Object.values(message.payload);
+      const schemaContent: MSBotFramework.SchemaContent[] = valuesByPlatform
+        .flatMap(value => Object.values(value).flatMap(b => b.blocks))
+        .filter(block => this.#schemaMap.has(block.component_type))
+        .map(this.#createSchemaForContent);
+      if (!schemaContent.length) {
+        return payloads;
+      }
+      return {
+        ...payloads,
+        [message.message_id]: schemaContent,
+      };
+    }, {}) as { [messageId: string]: MSBotFramework.SchemaContent; };
   };
   /**
    * Generates `.lg` file content and `.json` schema content for applicable blocks.
    * @param resources Object containing botmock resources.
    */
   #rootTransformation = (resources: Resources): DataTransformation => {
-    const payloadsContainingSchemaContent = (resources.board.board.messages as Botmock.Message[])
-      .reduce((payloads, message) => {
-        const valuesByPlatform = Object.values(message.payload);
-        const schemaContent: MSBotFramework.SchemaContent[] = valuesByPlatform
-          .flatMap(value => Object.values(value).flatMap(b => b.blocks))
-          .filter(block => this.#schemaMap.has(block.component_type))
-          .map(this.#createSchemaForContent);
-
-        if (!schemaContent.length) {
-          return payloads;
-        }
-        return {
-          ...payloads,
-          [message.message_id]: schemaContent,
-        };
-      }, {}) as { [messageId:string]: MSBotFramework.SchemaContent };
+    const payloadsContainingSchemaContent = this.#generateSchemaContent(resources.board.board.messages);
     return [
       {
         filename: `${resources.project.name}.lg`,
-        data: this.#createTemplatesFromProjectResources(resources),
+        data: this.#createTemplatesFromProjectResourcesAndSchemaContent(resources, payloadsContainingSchemaContent),
       },
-      ...Object.keys(payloadsContainingSchemaContent).map(key => ({
-        filename: `${key}.json`,
-        data: payloadsContainingSchemaContent[key],
+      ...Object.keys(payloadsContainingSchemaContent).map(messageId => ({
+        filename: `${messageId}.json`,
+        data: payloadsContainingSchemaContent[messageId],
       })),
     ];
   };
