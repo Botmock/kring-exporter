ddccaaa088dcb851f72f5b29de6c10e5c98599d7
Kevin Donahue <nonnontrivial@gmail.com>
2020-06-24 13:57:50 -0400

    use bot framework exporter

--- index.ts
+++ index.ts
@@ -3,8 +3,14 @@ import {
   FileWriter,
   Kind,
   ProjectReference,
+  BotFrameworkExporter,
 } from "@botmock/export";
-import { KringExporter } from "./exporter";
+
+const projectReference: ProjectReference = {
+  teamId: process.env.TEAM_ID as string,
+  projectId: process.env.PROJECT_ID as string,
+  boardId: process.env.BOARD_ID as string,
+};
 
 /**
  * @example
@@ -13,17 +19,9 @@ import { KringExporter } from "./exporter";
  * ```
  */
 async function main(): Promise<void> {
-  const projectReference: ProjectReference = {
-    teamId: process.env.TEAM_ID as string,
-    projectId: process.env.PROJECT_ID as string,
-    boardId: process.env.BOARD_ID as string,
-  };
-
-  const exporter = new KringExporter({ token: process.env.TOKEN as string });
+  const exporter = new BotFrameworkExporter({ token: process.env.TOKEN as string });
   const { data } = await exporter.exportProjectUnderDataTransformations({ projectReference });
-
-  const writeResult = await (new FileWriter()).writeAllResourcesToFiles({ data });
-
+  const writeResult = await (new FileWriter({ directoryRoot: "./language-generation" })).writeAllResourcesToFiles({ data });
   if (writeResult.kind !== Kind.OK) {
     console.error(writeResult.value);
   } else {
